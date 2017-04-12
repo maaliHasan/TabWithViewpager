05b3fd0c3e53e8f9e90b69e1d405fc089f8fdcad
Maali Hasan <mhasan@asaltech.com>
2017-04-12 17:33:56 +0300

    one main activity with three tabs and a viewpager created

---
+++ app/src/main/java/com/example/mhasan/tabwithviewpager/TabThree.java
@@ -0,0 +1,20 @@
+package com.example.mhasan.tabwithviewpager;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mhasan on 4/12/2017.
+ */
+
+public class TabThree extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab3,container,false);
+    }
+}
