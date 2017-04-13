31dcfd58e15982fb4f648ac4f8248efe8903c55e
Maali Hasan <mhasan@asaltech.com>
2017-04-13 10:14:08 +0300

    add icon to tab and edit its title

--- app/src/main/java/com/example/mhasan/tabwithviewpager/MainActivity.java
+++ app/src/main/java/com/example/mhasan/tabwithviewpager/MainActivity.java
@@ -1,22 +1,15 @@
 package com.example.mhasan.tabwithviewpager;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -120,11 +113,11 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Camera";
                 case 1:
-                    return "SECTION 2";
+                    return "Video";
                 case 2:
-                    return "SECTION 3";
+                    return "Record";
             }
             return null;
         }
